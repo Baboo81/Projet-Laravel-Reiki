463b0e15bda1874a4aabaa403a69d2d12d20a217
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-06-06 14:25:11 +0200

    Added img

--- Reiki/resources/views/home.blade.php
+++ Reiki/resources/views/home.blade.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="./app.css">
-        <title>Mon mini-blog</title>
+        <link rel="stylesheet" href="css/style.css">
+        <title>Le Reiki un art de vivre </title>
     </head>
     <body>
         <h1>Le Reiki</h1>
