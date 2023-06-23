317ea0405be19ed489183aed58a826659b3aab1c
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-06-23 18:36:52 +0200

    link, sass

--- Reiki/resources/views/layouts/app.blade.php
+++ Reiki/resources/views/layouts/app.blade.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Le Reiki, un art de vivre</title>
+    <!--Lien vers le css-->
+    {{ asset('css/app.css') }}
 </head>
     <body>
         @include('partials.navbar')
         <!--Contenu dynamique-->
         @yield('content')
+        <!--Lien vers le fichier js-->
+        {{ asset('js/app.js') }}
     </body>
 </html>
--- Reiki/webpack.mix.js
+++ Reiki/webpack.mix.js
@@ -3,6 +3,6 @@ const mix = require('laravel-mix');
 //Le webpack va permettre de compiler le js ainsi que le css se trouvant dans resources 
 //pour le placer compilé dans le dossier public:
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css', [
+    .sass('resources/scss/app.scss', 'public/css/app.css', [
         //
     ]);
