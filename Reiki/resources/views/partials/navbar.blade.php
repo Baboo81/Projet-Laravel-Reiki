3dde6084b84eca4b8703226da1e910e0b6a9fcd8
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-06-26 11:28:57 +0200

    Added img

--- Reiki/resources/views/partials/navbar.blade.php
+++ Reiki/resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <ul>
     <li><a href=" {{ route('welcome') }} ">Home</a></li>
-    <li><a href=" {{ route('introduction') }} ">Origine</a></li>
+    <li><a href=" {{ route('introduction') }} ">Introduction</a></li>
     <li><a href=" {{ route('contact') }} ">Contact</a></li>
 </ul>
