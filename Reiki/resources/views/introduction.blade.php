93d51d6cb5059471944176589574f0a9e5c03a52
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-07-11 14:12:15 +0200

    Added content

--- Reiki/resources/views/introduction.blade.php
+++ Reiki/resources/views/introduction.blade.php
@@ -4,7 +4,7 @@
         <div id="mainContent">
             <h1 id="titleIntro"> {{ $title }} </h1>
             <p id="contentIntro"> {{ $content }} </p>
-            <img src="./img/chrysanthem.jpg" alt="">
+            <img src="./img/chrysanthem.jpg" alt="image de chrysanthèmes">
         </div>
 @endsection
  
