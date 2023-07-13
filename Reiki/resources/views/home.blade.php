792a0782e0bc2dfe59b982c6ebb2be6acf27d8ec
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-07-13 16:13:09 +0200

    Added CSS

--- Reiki/resources/views/home.blade.php
+++ Reiki/resources/views/home.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    <section>
         <div id="titles">
             @foreach($posts as  $post)
                 <h1 id="mainTitle">{{ $post }}</h1>
             @endforeach
         </div>
+    </section>
 @endsection
 
 
