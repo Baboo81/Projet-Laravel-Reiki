6017c96c256d2e0e5a53968b74d10c7cbc8727b7
Chris Rodriguez Perez <magicbaboo@gmail.com>
2023-07-13 20:13:56 +0200

    Created Controller admin

--- Reiki/routes/admin.php
+++ Reiki/routes/admin.php
@@ -5,14 +5,14 @@
  */
 
  Route::prefix ('adb')->group(function () {
-    Route::get('/', [PostController::class, 'index'])->name('welcome');
+    Route::get('/', [AdminController::class, 'index'])->name('welcome');
     //La route écoute la home page, name sert à nommer les routes
     
-    Route::get('/introduction', [PostController::class, 'intro'])->name('introduction');
+    Route::get('/introduction', [AdminController::class, 'intro'])->name('introduction');
 
-    Route::get('/techniques', [PostController::class, 'tech'])->name('techniques');
+    Route::get('/techniques', [AdminController::class, 'tech'])->name('techniques');
 
-    Route::get('/contact', [PostController::class, 'form'])->name('contact');
+    Route::get('/contact', [AdminController::class, 'form'])->name('contact');
 
-    Route::get('/parametros/{param_1}/{param_2}/{param_3}', [PostController::class, 'parametros'])->name('parametros');
+    Route::get('/parametros/{param_1}/{param_2}/{param_3}', [AdminController::class, 'parametros'])->name('parametros');
  });
